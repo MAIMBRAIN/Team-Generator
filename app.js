eb1956633509820fa20392ec33a7392e07b7ee9c
Avery Stahl <averystahl@hotmail.com>
2020-01-17 16:30:09 -0500

    Better function calling and organization

--- app.js
+++ app.js
@@ -25,38 +25,9 @@ const teamMembers = [];
 // make a main app function that holds the logic
 function mainApp() 
 {
-    // A function that creates the team
-    function createTeam()
-    {
-        // There will only be one manager so we will call the manager() first
-        manager();
+    // There will only be one manager so we will call the manager() first
+    manager();
 
-        //use inquirer to allow the user the options to add engineer, intern, 
-        //or if you dont want to add another member
-        inquirer.prompt([
-            {
-                name: "createTeam",
-                type: "list",
-                message: "Would you like to add a team member?",
-                choices: ["Engineer", "Intern", "No"]
-            }
-        ]).then(res)
-        {
-            if(res.createTeam === "Engineer")
-            {
-                engineer();
-            }
-            else if(res.createTeam === "Intern")
-            {
-                intern();
-            }
-            else
-            {
-                return;
-            }
-        }
-    }
-    
     // A function that creates the manager
     function manager()
     {
@@ -65,7 +36,7 @@ function mainApp()
             {
                 name: "name",
                 type: "input",
-                message: "What is your name?"
+                message: "What is the manager's name?"
             },
             {
                 name: "email",
@@ -79,11 +50,68 @@ function mainApp()
             }
         ).then(res)
         {
+            // Create a new manager
             const manager = new Manager(res.name, indexOf("manager"), res.email, res.officeNum)
 
+            // Then add the new manager to the team
             teamMembers.push(manager);
+
+            // Finally, ask to add more employees
+            createTeam();
         }
     }
+
+    // A function that creates the team
+    function createTeam()
+    {
+        //use inquirer to allow the user the options to add engineer, intern, 
+        //or if you dont want to add another member
+        inquirer.prompt([
+            {
+                name: "createTeam",
+                type: "list",
+                message: "Would you like to add a team member?",
+                choices: ["Engineer", "Intern", "No"]
+            }
+        ]).then(res)
+        {
+            // Try using a switch!
+            switch (res.createTeam)
+            {
+                // If the response is "Engineer" then run the engineer function
+                case "Engineer": engineer();
+                    break;
+                
+                // If the response is "Intern" then run the engineer function
+                case "Intern": intern();
+                    break;
+
+                // Otherwise finish and build the team
+                default: buildTeam();
+                    break;
+            }
+
+            //  Working If statement
+            // If the response is "Engineer" then run the engineer function
+            // if(res.createTeam === "Engineer")
+            // {
+            //     engineer();
+            // }
+            // If the response is "Intern" then run the engineer function
+            // else if(res.createTeam === "Intern")
+            // {
+            //     intern();
+            // }
+            // Otherwise finish and build the team
+            // else
+            // {
+            //     buildTeam();
+            // }
+            console.log(teamMembers);
+        }
+    }
+    
+    
 
     // A function that creates the engineer
     function engineer()
@@ -107,9 +135,14 @@ function mainApp()
             }
         ).then(res)
         {
+            // Create a new Engineer
             const eng = new Engineer(res.name, indexOf("eng"), res.email, res.github)
 
+            // Then add the Engineer to the team
             teamMembers.push(eng)
+
+            // Ask if the user would like to add more team members
+            createTeam();
         }
     }
   
@@ -135,23 +168,23 @@ function mainApp()
             }
         ).then(res)
         {
+            // Create a new intern
             const intern = new Intern(res.name, indexOf("intern"), res.email, res.school);
 
+            // Add the intern to the team
             teamMembers.push(intern);
+
+            // Ask if the user would like to add more team members
+            createTeam();
         }
     }
 
     // This function takes the team members that was added to the teamMembers array and will create an html file
-    // and send it the to the out put folder
+    // and send it the to the output folder
     function buildTeam() 
     {
         fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
     }
-
-    buildTeam();
-    console.log(teamMembers);
 }
 
-
 mainApp();
-
